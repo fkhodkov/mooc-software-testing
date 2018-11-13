bf564abf0ba1dfb3e04f3753e5f874b6f58311f6
Fedor Khodkov <fkhodkov@gmail.com>
2018-11-13 16:36:25 +0300

    Fixed and tested CaesarShiftCipher

--- src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
+++ src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
@@ -12,8 +12,9 @@ public class CaesarShiftCipher {
         for(int i = 0; i < length; i++){
             currentChar = message.charAt(i);
            
-            sb.append(currentChar);
-            if (currentChar > 'z' || currentChar < 'a') {
+            if (currentChar == ' ') {
+                currentChar = (char) (currentChar - shift);
+            } else if (currentChar > 'z' || currentChar < 'a') {
                 return "invalid";
             } else if ((char) (currentChar + shift) > 'z') {
                 currentChar = (char) (currentChar - 26);
--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,70 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class CaesarShiftCipherTest {
+    @Test
+    public void invalidMessageDigit() {
+        String invalidMsg = "message 1";
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        Assertions.assertEquals("invalid",
+                                csc.CaesarShiftCipher(invalidMsg, 10));
+    }
+
+    @Test
+    public void invalidMessageCapital() {
+        String invalidMsg = "Message";
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        Assertions.assertEquals("invalid",
+                                csc.CaesarShiftCipher(invalidMsg, 10));
+    }
+
+    @Test
+    public void validMessageZeroShift() {
+        String message = "message";
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        Assertions.assertEquals("message",
+                                csc.CaesarShiftCipher(message, 0));
+    }
+
+    @Test
+    public void validMessageWithSpaceZeroShift() {
+        String message = "message with space";
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        Assertions.assertEquals("message with space",
+                                csc.CaesarShiftCipher(message, 0));
+    }
+
+    @Test
+    public void validMessage() {
+        String message = "abc";
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        Assertions.assertEquals("def",
+                                csc.CaesarShiftCipher(message, 3));
+    }
+
+    @Test
+    public void validMessageWithSpace() {
+        String message = "abc def";
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        Assertions.assertEquals("def ghi",
+                                csc.CaesarShiftCipher(message, 3));
+    }
+
+    @Test
+    public void validMessageOverBoundary() {
+        String message = "xyz";
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        Assertions.assertEquals("abc",
+                                csc.CaesarShiftCipher(message, 3));
+    }
+
+    @Test
+    public void validMessageOverBoundaryNegative() {
+        String message = "abc";
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        Assertions.assertEquals("xyz",
+                                csc.CaesarShiftCipher(message, -3));
+    }
 }
