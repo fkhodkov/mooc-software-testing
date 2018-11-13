b64412f6151b1cb249579db6cedde4362b8b9467
Fedor Khodkov <fkhodkov@gmail.com>
2018-11-13 16:11:53 +0300

    GHappy: fixed bug and added test cases

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,34 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    @Test
+    public void xxggxx() {
+        GHappy gh = new GHappy();
+        boolean result = gh.gHappy("xxggxx");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void xxgxx() {
+        GHappy gh = new GHappy();
+        boolean result = gh.gHappy("xxgxx");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void xxggyygxx() {
+        GHappy gh = new GHappy();
+        boolean result = gh.gHappy("xxggyygxx");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void ggxxgg() {
+        GHappy gh = new GHappy();
+        boolean result = gh.gHappy("ggxxgg");
+        Assertions.assertTrue(result);
+    }
 }
