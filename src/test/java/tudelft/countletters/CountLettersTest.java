a29054c8a6174b03c384538bd293038d1171959b
Fedor Khodkov <fkhodkov@gmail.com>
2018-11-21 13:37:15 +0300

    Fixed and tested CountLetters

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -6,14 +6,50 @@ import org.junit.jupiter.api.Test;
 public class CountLettersTest {
 
     @Test
-    public void multipleMatchingWords() {
+    public void singleMatchingWordR() {
+        int words = new CountLetters().count("cdr");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void singleMatchingWordS() {
+        int words = new CountLetters().count("cats");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void nonMatchingWord() {
+        int words = new CountLetters().count("cat");
+        Assertions.assertEquals(0, words);
+    }
+
+    @Test
+    public void multipleMatchingWordsS() {
         int words = new CountLetters().count("cats|dogs");
         Assertions.assertEquals(2, words);
     }
 
     @Test
-    public void lastWordDoesNotMatch() {
+    public void lastWordDoesNotMatchS() {
         int words = new CountLetters().count("cats|dog");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void multipleMatchingWordsR() {
+        int words = new CountLetters().count("editor|compiler");
+        Assertions.assertEquals(2, words);
+    }
+
+    @Test
+    public void lastWordDoesNotMatchR() {
+        int words = new CountLetters().count("editor|compile");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void nonLastWordDoesNotMatch() {
+        int words = new CountLetters().count("compile|editor");
         Assertions.assertEquals(1, words);
     }
 
