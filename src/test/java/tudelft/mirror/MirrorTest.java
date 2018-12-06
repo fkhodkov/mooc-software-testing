380a959870c9dbe21665bce5c9e67615f3554c12
Fedor Khodkov <fkhodkov@gmail.com>
2018-12-06 10:04:33 +0300

    Tested and fixed Mirror class

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,33 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+
 public class MirrorTest {
+    void simpleTest(String argument, String result) {
+        Mirror mirror = new Mirror();
+        Assertions.assertEquals(result, mirror.mirrorEnds(argument));
+    }
+
+    @Test
+    void abXYZba() {
+        simpleTest("abXYZba", "ab");
+    }
+    @Test
+    void abca() {
+        simpleTest("abca", "a");
+    }
+    @Test
+    void aba() {
+        simpleTest("aba", "aba");
+    }
+    @Test
+    void aa() {
+        simpleTest("aa", "aa");
+    }
+    @Test
+    void abba() {
+        simpleTest("abba", "abba");
+    }
 }
